90f65b8d63ac38eddf6fbec88047b3fd6e0ec7a5
Jamie <83338941+JamiroFerrara@users.noreply.github.com>
2022-05-12 11:57:04 +0200

    implemented search bar

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "244a2539d2168d43"; }
+/******/ 		__webpack_require__.h = function() { return "d177202fa74a9fca"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Shop/ProductList.tsx
+++ components/Shop/ProductList.tsx
@@ -3,6 +3,8 @@ import { useState } from 'react'
 import { storefront } from '../../utils'
 
 const ProductList = ({title, products}) => {
+  const [searchTerm, setSearchTerm] = useState("")
+
   return (
     <>
         <div className="w-full px-4 py-10 mb-8 bg-gray-100 rounded sm:px-6 lg:max-w-7xl">
@@ -10,11 +12,18 @@ const ProductList = ({title, products}) => {
             <h2 className="text-2xl w-3/5 font-extrabold tracking-tight text-gray-900">
               {title}
             </h2>
-            <div className="title w-2/5 border rounded p-1 border-slate-900">Search</div>
+            <input className="title w-2/5 border rounded p-1 border-slate-900" placeholder='Search' onChange={event => {setSearchTerm(event.target.value)}}/>
           </div>
 
           <div className="grid grid-cols-1 mt-6 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-            {products.edges.map((item) => {
+            {products.edges.filter((val)=> {
+              console.log(val)
+              if (searchTerm == "") {
+                return val
+              } else if (val.node.title.toLowerCase().includes(searchTerm.toLowerCase())) {
+                return val
+              }
+            }).map((item) => {
               
               const product = item.node
               const image = product.images.edges[0].node
