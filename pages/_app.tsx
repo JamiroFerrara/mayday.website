2870ee9f1935dfe5662a6ddcfb7565228cb462fd
Jamie <83338941+JamiroFerrara@users.noreply.github.com>
2022-05-13 14:04:32 +0200

    Added google analitics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,27 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script strategy='lazyOnload' src='https://www.googletagmanager.com/gtag/js?id=G-BEET688L2W'/>
+      <Script strategy='lazyOnload'>
+        {
+          `
+           window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+           gtag('config', 'G-BEET688L2W');
+          `
+        }
+      </Script>
+
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
 
