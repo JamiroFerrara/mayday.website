c9037b19be6a5b0ace9bd26b79c9f008c6c04f48
Jamie <83338941+JamiroFerrara@users.noreply.github.com>
2022-06-29 13:40:11 +0200

    Added cart button in product item

--- src/components/Shop/ProductItem.tsx
+++ src/components/Shop/ProductItem.tsx
@@ -1,39 +1,53 @@
 import { formatPrice } from '../../utils/shopify/helpers'
 import Link from 'next/link'
 import styles from './products.module.scss'
+import { FaCartPlus } from 'react-icons/fa'
+import { addToCart } from '../../utils/shopify'
+import { ItemAddedNotification } from '../../utils/notifications'
 
 const ProductItem = ({ product, image }) => {
   const isPhysical = isProductPhysical(product);
   const isOutOfStock = isProductOutOfStock(isPhysical, product);
   console.log(isOutOfStock, product.title, product.totalInventory);
+  const variantId = product.variants.edges[0].node.id;
+
+  function addToCartClicked(){
+    addToCart(variantId);
+    ItemAddedNotification(product.title);
+  }
 
   return (
-    <Link key={product.id} href={`${isOutOfStock ? "" : "/products/" + product.handle}`} scroll={false}>
-      <div className={styles.container}>
-        <div  className="relative group">
-          <div className="w-full overflow-hidden bg-gray-200 rounded-md min-h-80 aspect-w-1 aspect-h-1 lg:aspect-none group-hover:opacity-75 lg:h-80">
-            <div className={`${isOutOfStock ? 'bg-red-500' : ''} object-cover object-center w-full h-full lg:h-full lg:w-full`} >
-              <div className={`${isOutOfStock ? '' : 'hidden'}  w-full rounded text-white pl-2`}>Out of Stock</div>
-              <img src={image.transformedSrc} alt={image.altText}
-                className={`${isOutOfStock ? 'opacity-50' : ''} object-cover object-center w-full h-full`}/>
+    <div className={styles.container}>
+        <Link key={product.id} href={`${isOutOfStock ? "" : "/products/" + product.handle}`} scroll={false}>
+          <div>
+            <div className="w-full overflow-hidden bg-gray-200 rounded-md min-h-80 aspect-w-1 aspect-h-1 lg:aspect-none group-hover:opacity-75 lg:h-80">
+              <div className={`${isOutOfStock ? 'bg-red-500' : ''} object-cover object-center w-full h-full lg:h-full lg:w-full`} >
+                <div className={`${isOutOfStock ? '' : 'hidden'}  w-full rounded text-white pl-2`}>Out of Stock</div>
+                <img src={image.transformedSrc} alt={image.altText}
+                  className={`${isOutOfStock ? 'opacity-50' : ''} object-cover object-center w-full h-full`}/>
+              </div>
             </div>
-          </div>
-          <div className="flex justify-between mt-4">
-            <div>
-              <h3 className={styles.itemTitle}>
-                <a href="#">
-                  <span aria-hidden="true" className="absolute inset-0"></span>
-                  {product.title}
-                </a>
-              </h3>
+            <div className="flex justify-between mt-4">
+              <div>
+                <h3 className={styles.itemTitle}>
+                  <a href="#">
+                    <span aria-hidden="true" className="absolute inset-0"></span>
+                    {product.title}
+                  </a>
+                </h3>
+              </div>
+              <p className={styles.itemPrice}>
+                {formatPrice(product.priceRange.minVariantPrice.amount)}
+              </p>
             </div>
-            <p className={styles.itemPrice}>
-              {formatPrice(product.priceRange.minVariantPrice.amount)}
-            </p>
+            <div className="h-8"></div>
           </div>
-        </div>
-      </div>
-    </Link>
+        </Link>
+
+        <button onClick={() => addToCartClicked()} className="hover:text-red-500 hover:-translate-y-1 transition absolute m-2 right-2 bottom-2">
+          <FaCartPlus size={25}/>
+        </button>
+    </div>
   )
 }
 
