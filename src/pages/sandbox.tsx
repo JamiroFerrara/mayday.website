13be49c7b6cbfac0cc3871ffd573294ec1295119
Jamie <83338941+JamiroFerrara@users.noreply.github.com>
2022-06-30 14:37:49 +0200

    Added audio player test in sandbox

--- src/pages/sandbox.tsx
+++ src/pages/sandbox.tsx
@@ -1,7 +1,12 @@
 import { Group, Button } from '@mantine/core';
 import { ItemAddedNotification } from '../utils/notifications'
+import { useRef } from 'react'
+
+const url = "https://d31xpgvwmv5tnx.cloudfront.net/Stiwie%20-%20Quadruplex.mp3";
 
 export default function SandboxPage(){
+const musicPlayers = useRef<HTMLAudioElement | undefined>( typeof Audio !== "undefined" ? new Audio(url) : undefined);
+musicPlayers.current?.play();
 
   return(
     <>
