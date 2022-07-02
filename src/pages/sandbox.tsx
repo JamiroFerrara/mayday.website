4593498968bcc3dfdd8ce5130f88c27329623e0c
JamiroFerrara <ferrarajamiro@hotmail.com>
2022-07-02 19:27:37 +0200

    Actually created mutation button in sandbox

--- src/pages/sandbox.tsx
+++ src/pages/sandbox.tsx
@@ -1,6 +1,7 @@
 import { Group, Button } from '@mantine/core';
 import { ItemAddedNotification } from '../utils/notifications'
 import { useRef } from 'react'
+import {trpc} from '../utils/trpc'
 
 const url = "https://d31xpgvwmv5tnx.cloudfront.net/Stiwie%20-%20Quadruplex.mp3";
 
@@ -8,15 +9,12 @@ export default function SandboxPage(){
 const musicPlayers = useRef<HTMLAudioElement | undefined>( typeof Audio !== "undefined" ? new Audio(url) : undefined);
 musicPlayers.current?.play();
 
+const userMutation = trpc.useMutation(["createUser"])
+
   return(
     <>
       <Group position="center">
-        <Button
-          variant="outline"
-          onClick={() => ItemAddedNotification()}
-        >
-          Show customized notification
-        </Button>
+        <button className='btn' onClick={() => userMutation.mutate({Username: "jamiro", Password: "admin", Email: "Jakthebest@hotmail.it"})}>Create User</button>
       </Group>
     </>
   )
