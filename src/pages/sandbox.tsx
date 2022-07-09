cfe223fbf185718584401947ae2926e90b171b2d
JamiroFerrara <ferrarajamiro@hotmail.com>
2022-07-09 16:11:06 +0200

    Removed audio player from sandbox

--- src/pages/sandbox.tsx
+++ src/pages/sandbox.tsx
@@ -6,8 +6,8 @@ import {trpc} from '../utils/trpc'
 const url = "https://d31xpgvwmv5tnx.cloudfront.net/Stiwie%20-%20Quadruplex.mp3";
 
 export default function SandboxPage(){
-const musicPlayers = useRef<HTMLAudioElement | undefined>( typeof Audio !== "undefined" ? new Audio(url) : undefined);
-musicPlayers.current?.play();
+// const musicPlayers = useRef<HTMLAudioElement | undefined>( typeof Audio !== "undefined" ? new Audio(url) : undefined);
+// musicPlayers.current?.play();
 
 const userMutation = trpc.useMutation(["createUser"])
 
