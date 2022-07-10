24464cbac75d131336f5e7dffc95e07c75ae0ba2
JamiroFerrara <ferrarajamiro@hotmail.com>
2022-07-10 16:29:49 +0200

    Styled waveform as soundcloud audio

--- src/components/Audio/waveform.jsx
+++ src/components/Audio/waveform.jsx
@@ -1,58 +1,101 @@
-import { useEffect, useRef, useState } from "react";
-import Link from "next/link";
+import { useEffect, useRef, useState } from 'react'
+import Link from 'next/link'
+import { Center } from '@mantine/core'
+import { BiPlay, BiPause, BiUpload } from 'react-icons/bi'
 
 const formWaveSurferOptions = (ref) => ({
   container: ref,
-  waveColor: "#eee",
-  progressColor: "#0178FF",
-  cursorColor: "OrangeRed",
+  waveColor: '#eee',
+  progressColor: 'OrangeRed',
+  cursorColor: 'OrangeRed',
   barWidth: 3,
-  barRadius: 3,
+  barGap: 4,
+  barRadius: 2,
   responsive: true,
   height: 150,
   normalize: true,
-  partialRender: true
-});
+  partialRender: true,
+  audioRate: 1,
+  hideScrollbar: true,
+})
 
-export default function IndexPage() {
-  const waveformRef = useRef(null);
-  const wavesurfer = useRef(null);
-  const [playing, setPlaying] = useState(false);
+// plugins: [
+    // WaveSurfer.cursor.create({
+        // showTime: true,
+        // opacity: 1,
+        // customShowTimeStyle: {
+            // 'background-color': '#000',
+            // color: '#fff',
+            // padding: '2px',
+            // 'font-size': '10px'
+        // }
+    // })
+// ]
 
-  const url =
-      "https://d31xpgvwmv5tnx.cloudfront.net/Stiwie%20-%20Quadruplex.mp3"
+export default function IndexPage({ url }) {
+  const waveformRef = useRef(null)
+  const wavesurfer = useRef(null)
+  const [playing, setPlaying] = useState(false)
 
   useEffect(() => {
-    create();
+    create()
 
     return () => {
       if (wavesurfer.current) {
-        wavesurfer.current.destroy();
+        wavesurfer.current.destroy()
       }
-    };
-  }, []);
+    }
+  }, [])
 
   const create = async () => {
-    const WaveSurfer = (await import("wavesurfer.js")).default;
+    const WaveSurfer = (await import('wavesurfer.js')).default
 
-    const options = formWaveSurferOptions(waveformRef.current);
-    wavesurfer.current = WaveSurfer.create(options);
+    const options = formWaveSurferOptions(waveformRef.current)
+    wavesurfer.current = WaveSurfer.create(options)
 
-    wavesurfer.current.load(url);
-  };
+    wavesurfer.current.load(url)
+  }
 
   const handlePlayPause = () => {
-    setPlaying(!playing);
-    wavesurfer.current.playPause();
-  };
+    setPlaying(!playing)
+    wavesurfer.current.playPause()
+  }
 
   return (
-    <div>
-      <div id="waveform" ref={waveformRef} />
-      <div className="controls">
-        <div onClick={handlePlayPause}>{!playing ? "Слушать" : "Пауза"}</div>
+    <Center className='space-x-4'>
+      <div onClick={handlePlayPause}>
+        {!playing ? 
+          player(playing)
+        : 
+          player(playing)
+        }
       </div>
-      <Link href={{ pathname: "/about" }}>xxx</Link>
-    </div>
-  );
+
+      <div className="w-11/12">
+        <div id="waveform" ref={waveformRef} />
+      </div>
+
+    </Center>
+  )
+}
+
+function player(playing) {
+  if (playing){
+    return (
+        <div className='ml-2 w-10 h-10 rounded-full bg-orange-500 text-white flex justify-center'>
+          <div className='flex flex-col justify-center'>
+            <BiPause size={24}/>
+          </div>
+        </div> 
+    )
+      
+  } else {
+    return (
+        <div className='ml-2 w-10 h-10 rounded-full bg-orange-500 text-white flex justify-center'>
+          <div className='flex flex-col justify-center'>
+            <BiPlay size={24}/>
+          </div>
+        </div> 
+    )
+  }
 }
--- src/pages/sandbox2.tsx
+++ src/pages/sandbox2.tsx
@@ -1,12 +1,10 @@
-import dynamic from 'next/dynamic'
-
-const PodcastPlayer = dynamic(
-    () => import('../components/Audio/waveform'),
-    { ssr: false }
-)
+import AudioPlayer from '../components/Audio/waveform'
 
 export default function Sandbox2(){
   return (
-    <PodcastPlayer/>
+    <div className='m-10'>
+      <AudioPlayer url="https://d31xpgvwmv5tnx.cloudfront.net/Stiwie%20-%20Quadruplex.mp3"/>
+      <AudioPlayer url="https://d31xpgvwmv5tnx.cloudfront.net/Kick_Test_1.wav"/>
+    </div>
   )
 }
