f21ee77c08b08ccb16cde76be4459655c547951e
Jamie <83338941+JamiroFerrara@users.noreply.github.com>
2022-07-11 11:47:02 +0200

    Styled slider

--- src/components/Audio/waveform.jsx
+++ src/components/Audio/waveform.jsx
@@ -1,9 +1,9 @@
-import {useState, useEffect, useRef } from 'react'
-import {Center, Slider} from '@mantine/core'
+import { useState, useEffect, useRef } from 'react'
+import { Center, Slider } from '@mantine/core'
 
-export default function IndexPage({ url, wavesurfer}) {
+export default function IndexPage({ url, wavesurfer }) {
   const waveformRef = useRef(null)
-  const [value, setValue] = useState(50);
+  const [value, setValue] = useState(50)
 
   useEffect(() => {
     create()
@@ -18,7 +18,7 @@ export default function IndexPage({ url, wavesurfer}) {
   useEffect(() => {
     const val = (value + 50) / 100
     console.log(val)
-    wavesurfer.current?.setPlaybackRate(val);
+    wavesurfer?.current?.setPlaybackRate(val);
   }, [value])
 
   const create = async () => {
@@ -46,16 +46,44 @@ export default function IndexPage({ url, wavesurfer}) {
   return (
     <div className="w-full">
       <div id="waveform" ref={waveformRef} />
-      <Center>
         {/* <button onClick={() => } className='btn m-4'>1x</button> */}
-        <Slider value={value} onChange={setValue}
+        <Slider
+          value={value}
+          onChange={setValue}
+          size={2}
+          labelTransition="fade"
           marks={[
             { value: 25, label: '75%' },
             { value: 50, label: '100%' },
             { value: 85, label: '125%' },
           ]}
-        className='w-full'></Slider>
-      </Center>
+          className="mt-2 w-full outline-none"
+          styles={(theme) => ({
+            bar: {
+              backgroundColor: '#EA580C',
+            },
+            mark: {
+              borderRadius: 6,
+              borderColor: theme.colors.blue[6]
+            },
+            markFilled: {
+              borderColor: theme.colors.blue[6],
+            },
+            markLabel: {
+              fontSize: theme.fontSizes.xs,
+              marginBottom: 5,
+              marginTop: 2,
+            },
+            thumb: {
+              height: 12,
+              width: 12,
+              borderColor: 'white',
+              backgroundColor: theme.white,
+              borderWidth: 1,
+              boxShadow: theme.shadows.sm,
+            },
+          })}
+        ></Slider>
     </div>
   )
 }
