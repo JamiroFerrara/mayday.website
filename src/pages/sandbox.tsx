e1e2205bc5621ad90d461d1ae04f4282269a191e
JamiroFerrara <ferrarajamiro@hotmail.com>
2022-08-10 21:21:43 +0200

    Removed delete quad

--- src/pages/sandbox.tsx
+++ src/pages/sandbox.tsx
@@ -3,22 +3,14 @@ import { ItemAddedNotification } from '../utils/notifications'
 import { useRef, useState, useEffect } from 'react'
 import { trpc } from '../utils/trpc'
 import {useRouter} from 'next/router'
-import { deleteQuad } from '../backend/aws/s3'
 
 export default function SandboxPage() {
   // const musicPlayers = useRef<HTMLAudioElement | undefined>( typeof Audio !== "undefined" ? new Audio(url) : undefined);
   // musicPlayers.current?.play();
   //
 
-  function handleClick(){
-    deleteQuad('dbwlp.png');
-  }
-
   return (
     <>
-      <Center className='mt-6'>
-        <button className='btn' onClick={() => handleClick()}>Delete AWS file</button>
-      </Center>
     </>
   )
 }
